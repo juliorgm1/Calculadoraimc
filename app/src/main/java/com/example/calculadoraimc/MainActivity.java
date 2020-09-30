1328facfe43068fb11f5a51e0ccb78afc210cc19
juliorgm1 <julio.developer1@gmail.com>
2020-09-30 17:47:00 -0300

    Comentários na MainActivity

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editPeso;
     private EditText editAltura;
-    private String sPeso;
-    private String sAltura;
     private Button btnCalcular;
 
     @Override
@@ -29,19 +27,29 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Aqui validamos os campos do formulário, caso algum não passe na validação
+                //o método retornará false.
                 if(!validaCampos()) return;
 
+                //Conseguimos capturar os dados apenas como string, para fazer o calculo é
+                //necessário transformar a string para double com o metodo Double.parseDouble()
                 double peso = Double.parseDouble(editPeso.getText().toString());
                 double altura = Double.parseDouble(editAltura.getText().toString());
 
-                double imc = calculaIMC(peso, altura);
+                //Calculando o IMC
+                double imc = peso / (altura*altura);
+
+                //Enviando o IMC para activity de exibição do resultado
                 enviaResultado(imc);
             }
         });
     }
 
     private boolean validaCampos() {
+        //O método isEmpty retorna TRUE se a string do EditText estiver vazia
         if (editPeso.getText().toString().isEmpty()) {
+            //utilizamos o método setError para indicar ao usuário, algum problema no preechimento
+            //do formulário
             editPeso.setError("Informe o peso");
             return false;
         }
@@ -54,9 +62,15 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
+        if (Double.parseDouble(editAltura.getText().toString())>=3) {
+            editAltura.setError("Altura deve ser superior a 3 Metros");
+            return false;
+        }
+
         return true;
     }
 
+    //Esse metodo recebe o valor do imc e envia para ResultadoActivity
     private void enviaResultado(double imc) {
         Intent intent = new Intent(this, ResultadoActivity.class);
         intent.putExtra("IMC", imc);
@@ -68,22 +82,19 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         editPeso.setText("");
         editAltura.setText("");
-
         editPeso.requestFocus();
     }
 
-
+    // Esse metodo calcula o IMC e retorna o resultado
     private double calculaIMC(double peso, double altura) {
         double resultado = peso / (altura*altura);
         return resultado;
     }
 
+    //Esse método carrega os elementos do layout que serão manipulados
     private void carregaCampos() {
         editAltura = findViewById(R.id.editTextAltura);
         editPeso = findViewById(R.id.editTextPeso);
-
-        sPeso = editPeso.getText().toString();
-        sAltura = editAltura.getText().toString();
         btnCalcular = findViewById(R.id.buttonCalcular);
     }
 }
